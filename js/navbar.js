5f7ea4ccf0017cd7d59811be6965ad7176afd6ec
ashna-24 <93767921+ashna-24@users.noreply.github.com>
2023-03-15 18:11:08 +0530

    Commit

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var menu = document.getElementById('navbar');
+  
+    if(menu.className === "topnav"){
+      menu.className += "responsive";
+    }
+    else{
+      menu.className = "topnav";
+    }
+  }
